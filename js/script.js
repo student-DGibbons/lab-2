65851f5fde8a5d9c70462dbad4aa713c2a276163
student-DGibbons <200230810@student.georgianc.on.ca>
2025-03-07 15:37:28 -0500

    Completed car3 initialization

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Getting the output element so that text content can be output
+const output = document.getElementById('output');
+
+
+/* Constructor function for creating the product */
+function Car(title, company, year, ageRating, collectionNumber, website, color, country, model, material, speed, wheels){
+    this.title = title;
+    this.company = company;
+    this.year = year;
+    this.ageRating = ageRating;
+    this.brand = brand;
+    this.collectionNumber = collectionNumber;
+    this.website = website;
+    this.color = color;
+    this.country = country;
+    this.model = model;
+    this.material = material;
+    this.speed = speed;
+
+    this.describe = function() {
+        let description = `The ${this.title} was produced by ${this.company} in ${this.year} in ${this.country} for ${this.brand}. It has an age rating of ${this.ageRating}, and more information can be
+        found on ${this.website}. The collection number is ${this.collectionNumber}, it has a ${this.color} color, it is a ${this.model} model, it is made of ${this.material}, and has a speed of ${this.speed}.`;
+        return description;
+    }
+}
+
+
+/* Instantiating 3 instances of 3 different cars using the constructor function*/
+const car1 = new Car();
+const car2 = new Car();
+const car3 = new Car("Rally Speciale", "Mattel", 2021, "3+", "Hot Wheels", 179, "shop.mattel.com", "blue", "Malaysia", "racecar", "plastic", 2);
+
+
+// Outputting text content using the describe function for each car
+output.textContent = car1.describe();
+output.textContent = car2.describe();
+output.textContent = car3.describe();
+
